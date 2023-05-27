9e6c921e501102e5eba09eabb649ca64841a8d20
jbinsanad1 <108614522+jbinsanad1@users.noreply.github.com>
2023-05-27 12:53:52 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+new fullpage('#fullpage', {
+    //options here
+    autoScrolling:true,
+    scrollHorizontally: true,
+    sectionsColor : ['#081b29', '#081b29','#112F43','#112F43'],
+    navigation: true,
+    slidesNavigation: true,
+    anchors:['firstPage', 'secondPage', 'thirdPage', 'finalPage'],
+    navigationTooltips:['Home', 'About', 'Services', 'Contact Us']
+});
