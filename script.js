685f952a88e9fd52150ac2cc4ea798c754ba12f1
jbinsanad1 <108614522+jbinsanad1@users.noreply.github.com>
2023-05-28 14:30:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,18 @@ new fullpage('#fullpage', {
     //options here
     autoScrolling:true,
     scrollHorizontally: true,
-    sectionsColor : ['#081b29', '#081b29','#112F43','#112F43'],
+    sectionsColor : ['#081b29', '#081b29','#081b29','#081b29'],
     navigation: true,
     slidesNavigation: true,
     anchors:['firstPage', 'secondPage', 'thirdPage', 'finalPage'],
     navigationTooltips:['Home', 'About', 'Services', 'Contact Us']
 });
+
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
