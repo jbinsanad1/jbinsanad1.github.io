a27ded0cafb4d624a041698cbf5e1d81b1b164f9
jbinsanad1 <108614522+jbinsanad1@users.noreply.github.com>
2023-05-28 16:17:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,5 +7,21 @@ new fullpage('#fullpage', {
     slidesNavigation: true,
     anchors:['firstPage', 'secondPage', 'thirdPage', 'finalPage'],
     navigationTooltips:['Home', 'About', 'Services', 'Contact Us']
+
+});
+
+const primaryNav = document.querySelector('.primary-navigation');
+const navToggle = document.querySelector('.mobile-nav-toggle')
+
+navToggle.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute("data-visible")
+
+    if (visibility === "false") {
+        primaryNav.setAttribute("data-visible", true);
+        navToggle.setAttribute("aria-expanded", true)
+    } else {
+        primaryNav.setAttribute("data-visible", false)
+        navToggle.setAttribute("aria-expanded", false)
+    }
 });
 
