0b4a006deb1ec4e9f6572a16bf481155489abffa
jbinsanad1 <108614522+jbinsanad1@users.noreply.github.com>
2023-05-28 14:38:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,11 +9,3 @@ new fullpage('#fullpage', {
     navigationTooltips:['Home', 'About', 'Services', 'Contact Us']
 });
 
-// toggle icon navbar
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
